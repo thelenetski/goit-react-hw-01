60f319ae401fa612dad02b09463d2a1c99fe1825
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-07-19 13:38:16 +0300

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,5 @@
 import css from './FriendList.module.css';
-import FriendListItem from './FriendListItem';
+import FriendListItem from '../FriendListItem/FriendListItem';
 
 const FriendList = ({ friends }) => {
   return (
